1a90572ee863d5efe743041f46f682f09d33a57a
Harshmahto <62500223+Harshmahto@users.noreply.github.com>
2026-05-31 17:00:09 +0530

    Add: add-two-numbers - submission-2

---
+++ Data Structures & Algorithms/add-two-numbers/submission-2.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <algorithm> // Required for std::reverse
+
+
+class Solution {  
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+        while(l1!=nullptr || l2!=nullptr || carry!=0){
+            int v1 = (l1!=nullptr)?l1->val:0;
+            int v2 = (l2!=nullptr)?l2->val:0;
+
+            int v = v1+v2+carry;
+            carry = v/10;
+            int digit = v%10;
+            
+            temp->next = new ListNode(digit);
+            temp = temp->next;
+            l1 = (l1 != nullptr)?l1->next:nullptr;
+            l2 = (l2 != nullptr)?l2->next:nullptr;
+        }
+
+        return dummy->next;
+    }
+
+};
